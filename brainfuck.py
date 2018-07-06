ca53fd54dda020b2c8078718b252739df8be2cfd
Viktor Pavlov <vvpavlov19@gmail.com>
2018-07-06 23:44:22 +0300

    added first version of execute_code function

--- brainfuck.py
+++ brainfuck.py
@@ -1,6 +1,5 @@
 """Simple Brainfuck Interpreter."""
 
-
 import os
 
 
@@ -26,8 +25,61 @@ def filter_file(file):
 
     return output
 
-    # TODO: function interpreting the commands
+
+def execute_code(code):
+    """
+    Execute(print) the cleaned up Brainfuck code.
+
+    :param code: string of Brainfuck code
+    :return:
+    """
+    pointer = 0
+    cells = [0]
+    output = ""
+    qnt = 1
+    flag = False
+    stack = []
+
+    for char in code:
+        if char == "[":
+            stack.append("[")
+            flag = True
+            qnt = cells[-1] if flag else 1
+        if char == "]" and stack:
+            stack.pop()
+        if not stack:
+            flag = False
+            qnt = 1
+        if char == ">":
+            pointer += 1
+            if len(cells) - 1 < pointer:
+                cells.append(0)
+        elif char == "<":
+            pointer -= 1 if pointer > 0 else 0
+        elif char == "+":
+            cells[pointer] += qnt
+        elif char == "-":
+            cells[pointer] -= qnt if cells[pointer] >= 0 else 0
+        elif char == ".":
+            output += chr(cells[pointer])
+
+#    for char in code:
+#        if char == ">":
+#            pointer += 1
+#            cells[pointer] = 0 if len(cells) < pointer else cells[pointer]
+#        elif char == "<":
+#            pointer -= 1 if pointer > 0 else 0
+#        elif char == "+":
+#            cells[pointer] += 1
+#        elif char == "-":
+#            cells[pointer] -= 1 if cells[pointer] >= 0 else 0
+#        elif char == ".":
+#            output += chr(cells[pointer])
+
+    return output
 
 
 if __name__ == '__main__':
     print(filter_file("hello_world.bf"))
+    print(execute_code("++++++++[>++[>+++++>+++++++<<-]<-]>>++++++.>-------.++.+++++++++.-----.+++."))
+    print(execute_code("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+<<<<<<<-]>>.>---.+++++++..+++.>>.<-.+++.------.--------.>>+.>++."))
